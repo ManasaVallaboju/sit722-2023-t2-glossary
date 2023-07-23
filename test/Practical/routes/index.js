13a194d059cd05a503bcf9280db98e9d66f64095
geeks <s223001068@deakin.edu.au>
2023-07-23 20:39:04 +1000

    co

--- test/Practical/routes/index.js
+++ test/Practical/routes/index.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  fs.readFile('data.json', (err, data) => {
-    if (err) throw err;
-    let glossary = JSON.parse(data);
-    res.render('index', { title: 'SIT722 DevOps Glossary', subheading: 'Below is the table containing cllection of DevOps terms and their descriptions with references' });
-  });
+  res.render('index', { title: 'Sit722- - Software Deployment And Operation', subheading: 'Below is the table containing collection of DevOps terms and their descriptions with references' });
 });
 
 module.exports = router;
